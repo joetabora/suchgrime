5a0397ed7b60a3ce604c9ab1e3a87ab578c4b0f9
Joseph Tabora <joetabora@gmail.com>
2025-12-24 21:51:11 -0600

    perf(step-1): add Google Fonts preconnect hints
    Adds preconnect to fonts.googleapis.com and fonts.gstatic.com
    Expected improvement: ~1,500ms faster font loading
    Risk: Very low - just HTML link tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,6 +74,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${spaceGrotesk.variable} ${bebasNeue.variable}`}>
       <head>
+        {/* Preconnect to Google Fonts - speeds up font loading */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
